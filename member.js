5af213710507d938c688ae11a3421d860748a043
jiruthayaraj <jiruthayarajt@gmail.com>
2024-06-11 11:40:25 +0000

    2nd function

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node",
+    "MongoDB",
+    "Express",
+    "Python",
+    "Django",
+    "Flask",
+  ];
+  console.log(skills);
+}
